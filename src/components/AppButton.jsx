01dd082004a3dfd5fef7e72d863bfb7f240314e1
ovodo <ovdizzle@gmail.com>
2024-10-16 16:42:30 +0100

    improved desing on hero section

--- src/components/AppButton.jsx
+++ src/components/AppButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AppButton = ({ title }) => {
   return (
-    <button className='font-semibold text-base sm:text-[1.5vw] w-[118px] center text-app_blue h-[50px] sm:h-[80px] sm:w-[12vw] py-[2.6vh] rounded-[8px] sm:rounded-[12px] bg-app_yellow hover:-translate-x-[2vw] active:scale-90 duration-150'>
+    <button className='font-semibold text-base sm:text-[1.5vw] w-[118px] center text-app_blue h-[50px] sm:h-[80px] sm:w-[12vw]  rounded-[8px] sm:rounded-[12px] bg-app_yellow hover:-translate-x-[2vw] active:scale-90 duration-150'>
       {title}
     </button>
   );
